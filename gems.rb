7cd8d09ea6ee6025aff6d6313322a981dfb87182
Remo Mueller <remosm@gmail.com>
2017-12-31 14:39:15 -0500

    Minor changes to gemfile

--- gems.rb
+++ gems.rb
@@ -12,17 +12,17 @@ gem "bootstrap-sass",       "~> 3.3.7"
 gem "carrierwave",          "~> 1.2.1"
 gem "devise",               "~> 4.4.0"
 gem "figaro",               "~> 1.1.1"
+gem "font-awesome-rails",   "~> 4.7.0"
 gem "haml",                 "~> 5.0.4"
 gem "kaminari",             "~> 1.1.1"
-gem "font-awesome-rails"
 
 # Rails Defaults
 gem "coffee-rails",         "~> 4.2"
 gem "sass-rails",           "~> 5.0"
 gem "uglifier",             ">= 1.3.0"
 
-gem "jbuilder",             "~> 2.0"
-gem "jquery-rails",         "~> 4.2.1"
+gem "jbuilder",             "~> 2.5"
+gem "jquery-rails",         "~> 4.3.1"
 gem "turbolinks",           "~> 5"
 
 # # Action Cable dependencies for the Redis adapter
